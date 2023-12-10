150e3d61e08ebafd4dad8d5073cfafee0ca13ad4
WIllHu8i <119438873+WIllHu8i@users.noreply.github.com>
2023-12-10 11:55:04 -0500

    Add files via upload

---
+++ class graph, BFS and DFS:.py
@@ -0,0 +1,121 @@
+class Vertex:
+    def __init__(self, key):
+        self.key = key
+        self.neighbors = {}
+
+    def get_neighbor(self, other):
+        return self.neighbors.get(other, None)
+
+    def set_neighbor(self, other, weight=0):
+        self.neighbors[other] = weight
+
+    def __repr__(self):
+        return f"Vertex({self.key})"
+
+    def __str__(self):
+        return (
+            str(self.key)
+            + " connected to: "
+            + str([x.key for x in self.neighbors])
+        )
+
+    def get_neighbors(self):
+        return self.neighbors.keys()
+
+    def get_key(self):
+        return self.key
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def set_vertex(self, key):
+        self.vertices[key] = Vertex(key)
+
+    def get_vertex(self, key):
+        return self.vertices.get(key, None)
+
+    def __contains__(self, key):
+        return key in self.vertices
+
+    def add_edge(self, from_vert, to_vert, weight=0):
+        if from_vert not in self.vertices:
+            self.set_vertex(from_vert)
+        if to_vert not in self.vertices:
+            self.set_vertex(to_vert)
+        self.vertices[from_vert].set_neighbor(self.vertices[to_vert], weight)
+
+    def get_vertices(self):
+        return self.vertices.keys()
+
+    def __iter__(self):
+        return iter(self.vertices.values())
+
+g = Graph()
+for i in range(6):
+    g.set_vertex(i)
+print(g.vertices)
+g.add_edge(0, 1, 5)
+g.add_edge(0, 5, 2)
+g.add_edge(1, 2, 4)
+g.add_edge(2, 3, 9)
+g.add_edge(3, 4, 7)
+g.add_edge(3, 5, 3)
+g.add_edge(4, 0, 1)
+g.add_edge(5, 4, 8)
+g.add_edge(5, 2, 1)
+for v in g:
+    for w in v.get_neighbors():
+        print(f"({v.get_key()}, {w.get_key()})")
+
+
+
+from pythonds3.basic import Queue
+from pythonds3.graphs import Graph
+
+
+def bfs(start):
+    start.distance = 0
+    start.previous = None
+    vert_queue = Queue()
+    vert_queue.enqueue(start)
+    while vert_queue.size() > 0:
+        current = vert_queue.dequeue()
+        for neighbor in current.get_neighbors():
+            if neighbor.color == "white":
+                neighbor.color = "gray"
+                neighbor.distance = current.distance + 1
+                neighbor.previous = current
+                vert_queue.enqueue(neighbor)
+        current.color = "black"
+
+
+
+
+from pythonds3.graphs import Graph
+
+
+class DFSGraph(Graph):
+    def __init__(self):
+        super().__init__()
+        self.time = 0
+
+    def dfs(self):
+        for vertex in self:
+            vertex.color = "white"
+            vertex.previous = -1
+        for vertex in self:
+            if vertex.color == "white":
+                self.dfs_visit(vertex)
+
+    def dfs_visit(self, start_vertex):
+        start_vertex.color = "gray"
+        self.time = self.time + 1
+        start_vertex.discovery_time = self.time
+        for next_vertex in start_vertex.get_neighbors():
+            if next_vertex.color == "white":
+                next_vertex.previous = start_vertex
+                self.dfs_visit(next_vertex)
+        start_vertex.color = "black"
+        self.time = self.time + 1
+        start_vertex.closing_time = self.time
